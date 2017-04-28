6dbbaf6b861b6521bd52f460f558e5e6272530a8
Karthik <kop@qlik.com>
2017-04-28 15:55:19 +0530

    better logging and dockerize

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var AppModel = require('./api/models/WIMDModel');
+var logger = require('morgan');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/DriverDB');
+// For local deployment change this to mongoose.connect('mongodb://localhost/DriverDB');
+mongoose.connect('mongodb://database/DriverDB');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,7 +27,7 @@ app.listen(port);
 console.log('This RESTful API has started on:  ' + port);
 
 if (process.env.NODE_ENV !== 'test') {
-  app.use(express.logger());
+  app.use(logger);
 }
 
 module.exports = app;
